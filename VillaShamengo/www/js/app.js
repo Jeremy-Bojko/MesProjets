2e156dc98bf165930ac3802090e14fe041ecc4d4
cowboyyyy <je.bojko@gmail.com>
2015-10-02 12:34:21 +0200

    Gestions des Ibeacon fonctionnelle

--- VillaShamengo/www/js/app.js
+++ VillaShamengo/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('VillaShamengo', ['ionic', 'VillaShamengo.controllers', 'VillaShamengo.services','ngCordova','ngCordovaBeacon'])
+angular.module('VillaShamengo', ['ionic', 'VillaShamengo.controllers', 'VillaShamengo.services','ngCordova','ngCordovaBeacon '])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
